484e7da727634153b032aa4799b9c7958bc81be3
houjie1212 <houjie1212@sina.com>
2016-05-05 14:45:55 +0800

    ai实现

--- script/chess.js
+++ script/chess.js
@@ -6,6 +6,9 @@ var chessBoard_margin = 15;		//棋盘到边框巨鹿
 var chessBoard_boxLength = 30;	//棋盘格子边长
 var chess_adius = 13;			//棋子半径
 
+//游戏是否结束
+var over = false;
+
 //第一步黑子
 var me = true;
 context.strokeStyle = "#BFBFBF";
@@ -125,20 +128,118 @@ drawChessBoard();
 // oneStep(1, 1, false);
 
 chess.onclick = function(e) {
+	if(over) {
+		return;
+	}
+	if(!me) {
+		return;
+	}
 	var x = e.offsetX;
 	var y = e.offsetY;
 	var i = Math.floor(x/30);
 	var j = Math.floor(y/30);
 	if(chessBoard[i][j] == 0) {
 		oneStep(i, j, me);
-		if(me) {
-			chessBoard[i][j] = 1;
-		} else {
-			chessBoard[i][j] = 2;
+		chessBoard[i][j] = 1;
+		
+		for(var k=0; k<count; k++) {
+			if(wins[i][j][k]) {
+				myWin[k]++;
+				comWin[k] = 6;
+				if(myWin[k] == 5) {
+					alert("你赢了");
+					over = true;
+				}
+			}
 		}
-		me = !me;
+
+		if(!over) {
+			me = !me;
+			computerAi();
+		}
+	}
+}
+
+var computerAi = function() {
+	var myScore = [];
+	var comScore = [];
+	var max = 0;
+	var u = 0, v = 0;
+	for(var i=0; i<chessBoard_lineCnt; i++) {
+		myScore[i] = [];
+		comScore[i] = [];
+		for(var j=0; j<chessBoard_lineCnt; j++) {
+			myScore[i][j] = 0;
+			comScore[i][j] = 0;
+		}
 	}
 
+	for(var i=0; i<chessBoard_lineCnt; i++) {
+		for(var j=0; j<chessBoard_lineCnt; j++) {
+			if(chessBoard[i][j] == 0) {
+				for(var k=0; k<count; k++) {
+					if(myWin[k] == 1) {
+						myScore[i][j] += 200;
+					} else if(myWin[k] == 2) {
+						myScore[i][j] += 400;
+					} else if(myWin[k] == 3) {
+						myScore[i][j] += 2000;
+					} else if(myWin[k] == 4) {
+						myScore[i][j] += 10000;
+					}
+
+					if(comWin[k] == 1) {
+						comScore[i][j] += 220;
+					} else if(comWin[k] == 2) {
+						comScore[i][j] += 420;
+					} else if(comWin[k] == 2) {
+						comScore[i][j] += 2100;
+					} else if(comWin[k] == 2) {
+						comScore[i][j] += 20000;
+					}
+				}
+
+				if(myScore[i][j] > max) {
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				} else if(myScore[i][j] == max) {
+					if(comScore[i][j] > comScore[u][v]) {
+						u = i;
+						v = j;
+					}
+				}
+
+				if(comScore[i][j] > max) {
+					max = comScore[i][j];
+					u = i;
+					v = j;
+				} else if(comScore[i][j] == max) {
+					if(myScore[i][j] > myScore[u][v]) {
+						u = i;
+						v = j;
+					}
+				}
+			}
+		}
+	}
+
+	oneStep(u, v, false);
+	chessBoard[u][v] = 2;
+	for(var k=0; k<count; k++) {
+		if(wins[u][v][k]) {
+			comWin[k]++;
+			myWin[k] = 6;
+			if(comWin[k] == 5) {
+				alert("你输了");
+				over = true;
+			}
+		}
+	}
+
+	if(!over) {
+		me = !me;
+	}
 }
 
 
